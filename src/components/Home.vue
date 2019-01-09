ef18eb6b266b1fba25b21ce29cfa40250177e61a
Rafael <tolastarras@gmail.com>
2019-01-09 16:36:09 -0500

    list countries that use the selected currency

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="hello">
     <h1>Welcome</h1>
+    {{ exchangeRate }}
+    <ul v-for="country in countries" v-bind:key="country">
+      <li>{{ country }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      exchangeRate: 0,
+      countries: []
+    }
+  },
   created () {
-    console.log('created...')
-    this.getExchangeRate('USD', 'EUR')
+    this.exchangeRate = this.getExchangeRate('USD', 'EUR')
+    console.log('countries', this.getCountries('EUR'))
   },
   methods: {
     getExchangeRate (fromCurrency, toCurrency) {
-      let url = `http://apilayer.net/api/live?access_key=${process.env.VUE_APP_CURRENCY_LAYER_KEY}&currencies=${toCurrency}&source=${fromCurrency}&format=1`
-      this.$http.get(url)
-        .then(({ data }) => {
-          const rate = data.quotes
-          console.log(rate)
-        })
+      const API_KEY = process.env.VUE_APP_CURRENCY_LAYER_KEY
+      let url = `http://apilayer.net/api/live?access_key=${API_KEY}&currencies=${toCurrency}&source=${fromCurrency}&format=1`
+      this.$http.get(url).then(response => {
+        this.exchangeRate = response.data.quotes[`${fromCurrency}${toCurrency}`]
+      })
+    },
+    async getCountries (toCurrency) {
+      const response = await this.$http.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`)
+      this.countries = response.data.map(country => country.name)
     }
   }
 }
