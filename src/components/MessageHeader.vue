90a8af67a70da8edf966b573128bc41c9c55d189
Rafael <tolastarras@gmail.com>
2019-01-13 22:56:11 -0500

    format message header date

--- src/components/MessageHeader.vue
+++ src/components/MessageHeader.vue
@@ -14,8 +14,23 @@ export default {
     ...mapGetters(['currencyName']),
     ...mapState(['fromCurrency', 'toCurrency', 'fromCurrencyAmount', 'toCurrencyAmount']),
     currentDate () {
-      // let date = new Date
-      return 'Jan 9, 2:34AM EST'
+      let date = new Date
+
+      let tokens = date.toString().split(' ')
+      let timeTokens = tokens[4].split(':')
+
+      let ampm = 'AM'
+      let hour = timeTokens[0]
+
+      // validate hour and ampm values guessed
+      if (timeTokens[0] > 12) { 
+        hour = timeTokens[0] - 12
+        ampm = 'PM'
+      }
+
+      let time = `${hour}:${timeTokens[1]}${ampm} EST`
+
+      return `${tokens[1]} ${tokens[2]}, ${time}`
     }
   }
 }
